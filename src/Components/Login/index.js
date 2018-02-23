1fa1f9648fb390510d48c6416c8b951b87170594
Work <Work@Visalams-MacBook-Air.local>
2018-02-23 16:24:05 -0600

    final Day 1

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -5,101 +5,116 @@ import auth0 from 'auth0-js';
 import axios from 'axios';
 import { Auth0Lock } from 'auth0-lock';
 import { Auth0LockPasswordless } from 'auth0-lock';
+import './login.css';
+import { Button, Jumbotron } from 'react-bootstrap';
+import renderIf from 'render-if'
 
 
 
 class Login extends Component {
-  constructor(props){
-    super(props)
-    // this.state={
-    //   lock: new Auth0Lock("FL5V6YHdehEK6jGwX6dducPN28K0AtSh", "brackathon.auth0.com"),
-    // }
-    // this.state.lock.on("authenticated", function(authResult) {
-    //   console.log('inside this.state.lock.on');
-    //   console.log('value of authResult: ', authResult);
-    //   this.state.lock.getUserInfo(authResult.accessToken, function(error, profile) {
-    //     if (error) {
-    //       // Handle error
-    //       console.log("error in lock: ", error)
-    //       return;
-    //     }
-    //
-    //     localStorage.setItem("accessToken", authResult.accessToken);
-    //     localStorage.setItem("profile", JSON.stringify(profile));
-    //
-    //     // Update DOM
-    //   });
-    // });
-    this.state={
-      accessToken: null
-    }
-  }
+ constructor(props){
+   super(props)
+   // this.state={
+   //   lock: new Auth0Lock("FL5V6YHdehEK6jGwX6dducPN28K0AtSh", "brackathon.auth0.com"),
+   // }
+   // this.state.lock.on("authenticated", function(authResult) {
+   //   console.log('inside this.state.lock.on');
+   //   console.log('value of authResult: ', authResult);
+   //   this.state.lock.getUserInfo(authResult.accessToken, function(error, profile) {
+   //     if (error) {
+   //       // Handle error
+   //       console.log("error in lock: ", error)
+   //       return;
+   //     }
+   //
+   //     localStorage.setItem("accessToken", authResult.accessToken);
+   //     localStorage.setItem("profile", JSON.stringify(profile));
+   //
+   //     // Update DOM
+   //   });
+   // });
+   this.state={
+     accessToken: null,
+     picture: null,
+     name: null
+   }
+ }
 
-  componentDidMount(){
-    this.getProfile()
-  }
+ componentDidMount(){
+   this.getProfile()
+ }
 
-  getAccessToken() {
-      console.log('value of access_token: '+localStorage.getItem('access_token'));
-      if (localStorage.getItem('access_token')===null) {
-        console.log('No Access Token found');
-      }
-      this.setState({
-        accessToken: localStorage.getItem('access_token')
-      })
-    }
+ getAccessToken() {
+     console.log('value of access_token: '+localStorage.getItem('access_token'));
+     if (localStorage.getItem('access_token')===null) {
+       console.log('No Access Token found');
+     }
+     this.setState({
+       accessToken: localStorage.getItem('access_token')
+     })
+   }
 
-  getProfile() {
-    // console.log('inside getProfile');
-    // this.getAccessToken();
+ getProfile() {
+   // console.log('inside getProfile');
+   // this.getAccessToken();
 
-    if (localStorage.getItem('access_token')!=null){
-      this.auth0.client.userInfo(localStorage.getItem('access_token'), (err, profile) => {
-        console.log('profile: ', profile);
-        if (profile) {
-          this.userProfile = profile;
-          console.log('value of this.userProfile: ', this.userProfile);
-        }else{
-          console.log("profile err: ", err)
-        }
-      });
-    }
-  }
+   if (localStorage.getItem('access_token')!=null){
+     this.auth0.client.userInfo(localStorage.getItem('access_token'), (err, profile) => {
+       console.log('profile: ', profile);
+       if (profile) {
+         this.userProfile = profile;
+         console.log('value of this.userProfile: ', this.userProfile);
+         this.setState({picture: this.userProfile.picture});
+         this.setState({name: this.userProfile.nickname})
+       }else{
+         console.log("profile err: ", err)
+       }
+     });
+   }
+ }
 
-  auth0 = new auth0.WebAuth({
-      domain: 'brackathon.auth0.com',
-      clientID: 'FL5V6YHdehEK6jGwX6dducPN28K0AtSh',
-      redirectUri: 'http://localhost:3000/callback',
-      audience: 'https://brackathon.auth0.com/userinfo',
-      responseType: 'token id_token',
-      scope: 'openid profile'
-    });
+ auth0 = new auth0.WebAuth({
+     domain: 'brackathon.auth0.com',
+     clientID: 'FL5V6YHdehEK6jGwX6dducPN28K0AtSh',
+     redirectUri: 'http://localhost:3000/callback',
+     audience: 'https://brackathon.auth0.com/userinfo',
+     responseType: 'token id_token',
+     scope: 'openid profile'
+   });
 
-  login() {
-    this.auth0.authorize();
-  }
+ login() {
+   this.auth0.authorize();
+ }
 
-  render() {
+ render() {
 
-    return (
-      <div>
-        <p>
-            Hello login
-        </p>
-        <br/>
-        <button onClick={()=>{this.login()}}>Login Dawg</button>
-        <button onClick={()=>{this.authorizeAuth0()}}>authorizeAuth0</button>
-        <button onClick={()=>{this.handleAuthentication()}}>handleAuthentication</button>
-        <button onClick={()=>{this.setSession()}}>setSession</button>
-        <button onClick={()=>{this.isAuthenticated()}}>Login Dawg</button>
-        <button onClick={()=>{this.showStorage()}}>showStorage</button>
-        <button onClick={()=>{this.authorizeAuth0()}}>authorizeAuth0</button>
-        <button onClick={()=>{this.getProfile()}}>getProfile</button>
-        <button onClick={()=>{this.lockFunction()}}>lockFunction</button>
-        <button onClick={()=>{this.tryAPost()}}>tryAPost</button>
-      </div>
-    );
-  }
+   return (
+     <container className="loginLayout">
+         <Jumbotron className="jumboStyle">
+         <br/>
+         
+       
+         {renderIf(this.state.picture !== null)(
+           <div>
+             <img src = {this.state.picture} />
+           </div>
+           )}
+
+         {renderIf(this.state.picture == null)(
+           <div>
+             <button className="buttonStyle" onClick={()=>{this.login()}}>Login Dawg</button>
+           </div>
+           )}
+       
+
+         <div className="helloStuff">
+         <h1>Hello {this.state.name}</h1>
+       
+         </div>
+         </Jumbotron>;
+     </container>
+   );
+ }
 }
 
 export default Login;
